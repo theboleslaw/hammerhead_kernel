cd53e8963dea773c267c1ed280534bee80f2481e
ctfrommn <chadclough@gmail.com>
2014-04-16 23:42:55 +0200

    Use Martin's build script

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright (C) 2013 MartinRo
 # credits to sparksco for the SaberMod kernel buildscript as base
-# Build Script. Use bash to run this script, bash mako-kernel from source directory
+# Build Script. Use bash to run this script
 export MANUFACTURER=lge;
 export DEVICE=hammerhead;
 
@@ -18,9 +18,32 @@ export USE_CCACHE=1
 #export KERNELSRC=$DIRSRC;
 
 # Target gcc version
-export TARGET_GCC=4.8;
-export ARM_EABI_TOOLCHAIN=../../../prebuilts/gcc/linux-x86/arm/arm-eabi-4.8-sm;
-export PATH=$PATH:$ARM_EABI_TOOLCHAIN/bin:$ARM_EABI_TOOLCHAIN/arm-eabi/bin;
+#export TARGET_GCC=4.8;
+export TARGET_GCC=4.10;
+
+#the following lines one have some effect if you use 4.8 as TARGET_GCC
+#if you want to use cfx, uncomment next line
+#export TARGET_GCC_SUB=cfx;
+
+#default one : googles toolchain. comment it out if you want to use different one
+#export TARGET_GCC_SUB=google; 
+
+#if you want to use the old sabermod toolchain from github, uncomment the next line
+#export TARGET_GCC_SUB=sm_old;
+
+#if you want to use the most current sabermod toolchain from sourceforge, uncomment the next line
+#export TARGET_GCC_SUB=sm_new;
+
+#if you want to use the most current sabermod toolchain from sourceforge, uncomment the next line
+export TARGET_GCC_SUB=UBER;
+
+
+
+export ARM_EABI_TOOLCHAIN=../../../prebuilts/gcc/linux-x86/arm/arm-eabi-$TARGET_GCC/$TARGET_GCC_SUB;
+
+#export PATH=$ARM_EABI_TOOLCHAIN/arm-eabi/bin:$ARM_EABI_TOOLCHAIN/bin:$PATH;
+
+ echo 'Kernel buid with ' $ARM_EABI_TOOLCHAIN;
 
 # Build ID
 export LOCALVERSION=""
@@ -42,7 +65,7 @@ then
 fi;
 
 make hammerhead_defconfig;
-time make -j16;
+time make -j8;
 
 if [ -e ./arch/arm/boot/zImage-dtb ] ;
 then
